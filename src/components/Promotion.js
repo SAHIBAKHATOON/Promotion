4f01c90dcd5ef8aef81acef928b23dc00992372e
SAHIBAKHATOON <115203203+SAHIBAKHATOON@users.noreply.github.com>
2024-04-03 21:14:35 +0530

    first commit

---
+++ src/components/Promotion.js
@@ -0,0 +1,18 @@
+import React from 'react';
+ import './Promotion.css';
+
+ 
+const Promotion = () => {
+  return (
+    <>
+    <div className='img_section'> 
+     <div className='items'>
+        <p>Promote your YouTube Videos Now</p>
+        <button>Start Promotion</button>
+     </div>
+    </div>
+    </>
+  )
+}
+
+export default Promotion
